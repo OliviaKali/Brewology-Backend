eb831f4f4832c275eafa592b335442a33bee8e87
OKali <o.kalinowski27@gmail.com>
2021-03-05 15:03:21 -0500

    basic server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(express.static("app/public"));
+
+//Require Routes Here...
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`)
+})
